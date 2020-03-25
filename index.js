d7077733507d4f91b81d9b32a7c0f6790238ca72
daniel_s <35691859+stemmerSoSe19@users.noreply.github.com>
2020-03-25 16:53:52 +0100

    Umstellung auf queryable-DB
    - Modul "queryable" hinzugefügt
    - Datenbank mit einigen Einträgen erstellt
    - Ordner "db" enthält Datenbank
    - Ordner "img" enthält Bilddateien

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const express = require('express');
-const app = express();
+var express     = require('express');
+var queryable   = require('queryable');
+
+var app         = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
+
+//SETUP
+var db = queryable.open( "db/mydata.db" );
