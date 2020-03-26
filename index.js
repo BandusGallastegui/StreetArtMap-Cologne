2abbffdcd1ca229b74b616709feb6010fe78e38b
daniel_s <35691859+stemmerSoSe19@users.noreply.github.com>
2020-03-26 21:54:24 +0100

    Client Sendefunktion
    - Client kann jetzt POIs hinzufügen
    - Server fügt POIs der Datenbank hinzu
    - jQuery importiert
    - dies und das

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express     = require('express');
-const queryable   = require('queryable');
+const express = require('express');
+const queryable = require('queryable');
+const fs = require('fs');
 
 //DB SETUP
-var db = queryable.open( "db/mydata.db" );
+var db = queryable.open("db/mydata.db");
 
 //SERVER SETUP
 var app = express();
@@ -14,8 +15,39 @@ app.get('/api', (request, response) => {
     response.json(db.find());
 });
 
-/*app.post('/api', (request, response) => {
+app.post('/api', (request, response) => {
     const data = request.body;
-    response.json(data);
-});*/
+    db.insert(data);
+    db.save();
+});
 
+//DEVELOPMENT
+
+/*
+--- db format example ---
+
+{
+    "name":"Edelweißpiraten",
+    "artist":"Captain Borderline",
+    "viertel":"Ehrenfeld",
+    "lat":50.9516951,
+    "lon":6.9129999,
+    "img":"img/1.jpg"
+}
+*/
+
+/*
+--- initial db entry ---
+
+var imgPath = "img/1.jpg";
+var imgData = fs.readFileSync(imgPath,'base64');
+db.insert({
+    "name":"Edelweißpiraten",
+    "artist":"Captain Borderline",
+    "viertel":"Ehrenfeld",
+    "lat":50.9516951,
+    "lon":6.9129999,
+    "img":imgData
+});
+db.save();
+*/
